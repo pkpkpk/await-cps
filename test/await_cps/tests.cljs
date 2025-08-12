38b5e6eecb4d445f45cc742f90f5826451673a75
pat killean <pat.killean@gmail.com>
2025-08-11 22:15:58 -0400

    wip

--- test/await_cps/tests.cljs
+++ test/await_cps/tests.cljs
@@ -17,17 +17,18 @@
        (is (instance? js/Error (second @events)))))))
 
 (defn async-ok [r e] (js/setTimeout #(r :ok) 0))
+(defn async-err [r e] (js/setTimeout #(e :err) 0))
 
 (deftest await-async-defers
   (async done
     (testing "async defers to a later tick"
-      (let [events (atom nil)
-            r (fn [v]
-                (reset! events [:r v])
-                (is (= [:r :ok] @events))
-                (done))
-            e (fn [t] (throw (js/Error. "unexpected")))]
-        (and
-         (is (nil? @events))
-         (is (nil? ((afn [] (await async-ok)) r e))))))))
+      (let [fatal (fn [t] (throw (js/Error. (str "unexpected: " t))))
+            expect-err (fn [e]
+                         (is (= :err e))
+                         (done))
+            expect-ok (fn [v]
+                        (if-not (is (= :ok v))
+                          (done)
+                          (is (nil? ((afn [] (await async-err)) fatal expect-err)))))]
+        (is (nil? ((afn [] (await async-ok)) expect-ok fatal)))))))
 
